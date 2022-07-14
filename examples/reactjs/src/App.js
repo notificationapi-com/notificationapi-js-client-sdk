5e4a7dd9326ac6febb3cb15f9c5dc36f1f04fed3
Sahand Seifi <sahand.seifi@gmail.com>
2022-07-14 11:52:58 -0230

    Add reactjs example

---
+++ examples/reactjs/src/App.js
@@ -0,0 +1,11 @@
+import NotificationAPIComponent from './NotificationAPIComponent';
+
+function App() {
+  return (
+    <div className="App" style={{textAlign: 'center'}}>
+      <NotificationAPIComponent/>
+    </div>
+  );
+}
+
+export default App;
