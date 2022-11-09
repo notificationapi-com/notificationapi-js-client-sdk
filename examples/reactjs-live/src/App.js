ce0647b2afd5a22be963d22851903619d85d53a2
Sahand Seifi <sahand.seifi@gmail.com>
2022-11-09 11:32:11 -0330

    Create live tests that use the actual package

---
+++ examples/reactjs-live/src/App.js
@@ -0,0 +1,12 @@
+import NotificationAPIComponent from './NotificationAPIComponent';
+import 'notificationapi-js-client-sdk/dist/styles.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <NotificationAPIComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ examples/reactts-live/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NotificationAPIComponent from './NotificationAPIComponent';
+import 'notificationapi-js-client-sdk/dist/styles.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <NotificationAPIComponent />
+    </div>
+  );
+}
+
+export default App;
