870d1932a1c6df9862bc3b2d58d1dbf1f5510d9a
mbasadi <asadi.mohammadbagher@gmail.com>
2024-02-21 08:20:58 -0500

    A new envId without xss (One of the message for this envId had an alert html)

--- examples/reactjs-live/src/NotificationAPIComponent.js
+++ examples/reactjs-live/src/NotificationAPIComponent.js
@@ -7,7 +7,7 @@ const NotificationAPIComponent = memo((props) => {
 
   useEffect(() => {
     const notificationapi = new NotificationAPI({
-      clientId: '24nojpnrsdc53fkslha0roov05',
+      clientId: '54jas9ae4omlbbq3s0u0d9spui',
       userId: 'sahand'
     });
     notificationapi.showInApp({
