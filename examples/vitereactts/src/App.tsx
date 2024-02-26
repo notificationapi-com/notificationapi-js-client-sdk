77937ced2c03989b47c8773d43d66ecf072ed7fc
mbasadi <asadi.mohammadbagher@gmail.com>
2024-02-26 15:49:35 -0500

    test vite react in PR pipeline

---
+++ examples/vitereactts/src/App.tsx
@@ -0,0 +1,12 @@
+import 'notificationapi-js-client-sdk/dist/styles.css';
+import NotificationAPIComponent from './NotificationAPIComponent';
+
+function App() {
+  return (
+    <>
+      <NotificationAPIComponent userId="sahand" />
+    </>
+  );
+}
+
+export default App;
