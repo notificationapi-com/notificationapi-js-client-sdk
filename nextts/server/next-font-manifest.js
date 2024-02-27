3b97769fd31206f05c5ec0e03068709547aa5e21
Automated Action <noreply@notificationapi.com>
2024-02-27 15:49:12 -0500

    Updates

---
+++ nextts/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{"/NotificationAPIComponent":[],"/":[]},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":false}';
---
+++ nextts/static/chunks/pages/NotificationAPIComponent-749e6feb908d54b2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[463],{4123:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/NotificationAPIComponent",function(){return o(400)}])}},function(n){n.O(0,[991,400,888,774,179],function(){return n(n.s=4123)}),_N_E=n.O()}]);
