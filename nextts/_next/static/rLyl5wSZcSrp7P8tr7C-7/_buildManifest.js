805df7181df4e3b6a0c3836d2ffa2859e894a979
Automated Action <noreply@notificationapi.com>
2024-02-28 17:26:10 +0000

    Updates

--- nextts/_next/static/rLyl5wSZcSrp7P8tr7C-7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/05b35d4016f7675b.css","static/chunks/pages/index-e52e03daea9ab02c.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
