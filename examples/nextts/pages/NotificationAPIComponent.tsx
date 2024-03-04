2537d82182652feb0f624d7eb0cfa9ee3348aeea
mbasadi <asadi.mohammadbagher@gmail.com>
2024-03-04 10:56:02 -0500

    use example code

--- examples/nextts/pages/NotificationAPIComponent.tsx
+++ examples/nextts/pages/NotificationAPIComponent.tsx
@@ -1,21 +1,28 @@
-import NotificationAPI from 'notificationapi-js-client-sdk';
-import 'notificationapi-js-client-sdk/dist/styles.css';
-import { PopupPosition } from 'notificationapi-js-client-sdk/lib/interfaces';
-import { memo, useEffect } from 'react';
+'use client';
 
+import 'notificationapi-js-client-sdk/dist/styles.css';
+import { memo, useEffect } from 'react';
 const NotificationAPIComponent = memo((props: { userId: string }) => {
   useEffect(() => {
-    const notificationapi = new NotificationAPI({
-      clientId: '54jas9ae4omlbbq3s0u0d9spui',
-      userId: props.userId
-    });
-    notificationapi.showInApp({
-      root: 'CONTAINER_DIV_ID',
-      popupPosition: PopupPosition.BottomLeft
-    });
+    const loadNotificationAPI = async () => {
+      const NotificationAPI = (await import('notificationapi-js-client-sdk'))
+        .default;
+      const notificationapi = new NotificationAPI({
+        clientId: '54jas9ae4omlbbq3s0u0d9spui',
+        userId: props.userId
+      });
+      notificationapi.showInApp({
+        root: 'CONTAINER_DIV_ID'
+      });
+    };
+
+    // Call the async function
+    loadNotificationAPI();
   }, [props.userId]);
 
   return <div id="CONTAINER_DIV_ID"></div>;
 });
+
+NotificationAPIComponent.displayName = 'NotificationAPIComponent';
 
 export default NotificationAPIComponent;
