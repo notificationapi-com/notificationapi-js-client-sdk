7f2caf60ba3e497f5289a8b5379fb9370a4f36b8
mbasadi <asadi.mohammadbagher@gmail.com>
2024-03-04 12:04:36 -0500

    add nuxt locally

---
+++ examples/nuxt/nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  app: {
+    baseURL: '/notificationapi-js-client-sdk/nuxt/' // Set the base URL for the application
+  }
+});
